f9bc8ebbbcaa094471c3df876605a7849d8cab9e
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-05-06 16:31:32 -0300

    Inserir o Tipo Enumerável do Tipo da Mensagem da Exceção do Banco de Dados

---
+++ Back-End/src/main/java/com/senac/aesthetics/enums/TipoMensagemEnum.java
@@ -0,0 +1,16 @@
+package com.senac.aesthetics.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum TipoMensagemEnum {
+    
+    SUCESS("sucess"),
+    INFO("info"),
+    WARN("warn"),
+    ERROR("error");
+
+    private String tipo;
+}
