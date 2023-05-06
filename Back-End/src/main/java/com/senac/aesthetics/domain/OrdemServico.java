9c8caa2485113445b9141f4971fc3d150e06a9e8
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-05-06 15:18:57 -0300

    Inserir o Mapeamento da Classe OrdemServico

--- Back-End/src/main/java/com/senac/aesthetics/domain/OrdemServico.java
+++ Back-End/src/main/java/com/senac/aesthetics/domain/OrdemServico.java
@@ -1,14 +1,27 @@
 package com.senac.aesthetics.domain;
 
 import java.util.Date;
-import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.senac.aesthetics.domain.enums.StatusOrdemServicoEnum;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,24 +42,44 @@ public class OrdemServico {
     @Column(name = "ID_ORDEM_SERVICO")
     private Long id;
 
+    @Column(name = "DATA_HORA_INICIO", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @NotNull(message = "A Data e Horário do Início da Ordem de Serviço Deve Ser Informada!")
     private Date dataHoraInicio;
 
+    @Column(name = "DATA_HORA_TERMINO", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @NotNull(message = "A Data e Horário do Término da Ordem de Serviço Deve Ser Informada!")
     private Date dataHoraTermino;
 
-    private String status;
+    @Column(name = "STATUS", length = 15, nullable = false)
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O Status da Ordem de Serviço Deve Ser Informado!")
+    @Size(max = 15, message = "O Tamanho Máximo do Status da Ordem de Serviço é de 15 Caracteres!")
+    @Pattern(regexp = "^(ABERTO|CANCELADO|EM_EXECUCAO|CONCLUIDO)$", message = "O Status da Ordem de Serviço Só Pode Ser: ABERTO ou CANCELADO ou EM_EXECUCAO ou CONCLUIDO!")
+    private StatusOrdemServicoEnum status;
 
+    @Column(name = "VALOR", nullable = false, precision = 10, scale = 2)
+    @Digits(integer = 10, fraction = 2, message = "O Valor Máximo da Ordem de Serviço é de R$ 1.000.000,00 (1 Milhão)!")
+    @PositiveOrZero(message = "O Valor da Ordem de Serviço Deve Estar Entre R$ 0 (Zero) e R$ 1.000.000,00 (1 Milhão)!")
+    @NotNull(message = "O Valor da Ordem de Serviço Deve Ser Informado!")
     private Float valor;
 
     // Relacionamentos:
-
+    @OneToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "ID_AGENDAMENTO_FK", referencedColumnName = "ID_AGENDAMENTO")
     private Agendamento agendamento;
-
+    
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "ID_SERVICO_FK", referencedColumnName = "ID_SERVICO")
     private Servico servico;
 
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "ID_FUNCIONARIO_RESPONSAVEL_OS_FK", referencedColumnName = "ID_FUNCIONARIO")
     private Funcionario respOS;
 
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "ID_FUNCIONARIO_EXECUTA_SERVICO_FK", referencedColumnName = "ID_FUNCIONARIO")
     private Funcionario execServico;
-
-    private Set<ContaReceber> contasReceber;
     
 }
