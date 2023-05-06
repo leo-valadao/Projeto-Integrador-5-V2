9fdebbaae9ce696e615aeb4414eb0981a1e5126f
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-05-06 12:42:45 -0300

    Inserir o Mapeamento da Classe Cliente

--- Back-End/src/main/java/com/senac/aesthetics/domain/Cliente.java
+++ Back-End/src/main/java/com/senac/aesthetics/domain/Cliente.java
@@ -7,7 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,14 +23,21 @@ import lombok.Setter;
 // Java Persistence API:
 @Entity(name = "Clinte")
 @Table(name = "CLIENTES")
-public class Cliente extends Pessoa{
-    
+public class Cliente extends Pessoa {
+
     // Atributos:
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_CLIENTE")
     private Long id;
 
+    @Column(name = "ALERGIAS", length = 100)
+    @Size(max = 100, message = "O Tamanho Máximo das Alergias do Cliente é de 100 Caracteres!")
+    private String alergias;
+
     // Relacionamentos:
+    @OneToMany(orphanRemoval = false, mappedBy = "cliente")
+    @JoinColumn(name = "ID_AGENDAMENTO_FK", referencedColumnName = "ID_CLIENTE")
     private Set<Agendamento> agendamentos;
+
 }
