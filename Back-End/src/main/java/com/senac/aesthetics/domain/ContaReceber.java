84355dc2a876a5d10126aa6531296885712b4502
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-05-06 14:11:12 -0300

    Inserir o Mapeamento da Classe ContaReceber

--- Back-End/src/main/java/com/senac/aesthetics/domain/ContaReceber.java
+++ Back-End/src/main/java/com/senac/aesthetics/domain/ContaReceber.java
@@ -3,12 +3,26 @@ package com.senac.aesthetics.domain;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.senac.aesthetics.domain.enums.StatusContaReceberEnum;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,16 +43,30 @@ public class ContaReceber extends Conta {
     @Column(name = "ID_CONTA_RECEBER")
     private Long id;
 
+    @Column(name = "VALOR_RECEBIDO", precision = 10, scale = 2)
+    @Digits(integer = 10, fraction = 2, message = "O Valor Máximo de Recebimento da Conta a Receber é de R$ 1.000.000,00 (1 Milhão)!")
+    @PositiveOrZero(message = "O Valor Recebido da Conta a Receber Deve Estar Entre R$ 0 (Zero) e R$ 1.000.000,00 (1 Milhão)!")
     private Float valorRecebido;
 
+    @Column(name = "DATA_RECEBIMENTO")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    @PastOrPresent(message = "A Data de Recebimento da Conta a Receber Deve Ser Uma Data no Presente ou no Passado!")
     private Date recebimento;
 
-    private String status;
+    @Column(name = "STATUS", length = 15, nullable = false)
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O Status da Conta a Receber Deve Ser Informado!")
+    @Size(max = 15, message = "O Tamanho Máximo do Status da Conta a Receber é de 15 Caracteres!")
+    @Pattern(regexp = "^(ABERTO|CANCELADO|RECEBIDO)$", message = "O Status da Conta a Receber Só Pode Ser: ABERTO ou CANCELADO ou RECEBIDO!")
+    private StatusContaReceberEnum status;
 
     // Relacionamentos:
-
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "ID_ORDEM_SERVICO_FK", referencedColumnName = "ID_ORDEM_SERVICO")
     private OrdemServico ordemServico;
 
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "ID_CLIENTE_FK", referencedColumnName = "ID_CLIENTE")
     private Cliente cliente;
 
 }
