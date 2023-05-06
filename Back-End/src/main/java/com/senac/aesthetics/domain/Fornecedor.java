752da8ea3bc1f55509f6650b4d18fe713b85596b
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-05-06 13:59:55 -0300

    Inserir o Mapeamento da Classe Fornecedor

--- Back-End/src/main/java/com/senac/aesthetics/domain/Fornecedor.java
+++ Back-End/src/main/java/com/senac/aesthetics/domain/Fornecedor.java
@@ -1,10 +1,14 @@
 package com.senac.aesthetics.domain;
 
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,5 +29,10 @@ public class Fornecedor extends Pessoa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_FORNECEDOR")
     private Long id;
+
+    // Relacionamentos:
+    @OneToMany(orphanRemoval = false, mappedBy = "fornecedor")
+    @JoinColumn(name = "ID_CONTA_PAGAR_FK", referencedColumnName = "ID_CONTA_PAGAR")
+    private Set<ContaPagar> contasPagar;
     
 }
