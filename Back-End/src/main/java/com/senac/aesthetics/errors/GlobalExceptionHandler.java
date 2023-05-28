39145707d13dea8459699e465cde1cde4edf6e76
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-05-28 04:57:40 -0300

    Inserir um Global Handler Exception no Back-End

---
+++ Back-End/src/main/java/com/senac/aesthetics/errors/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.senac.aesthetics.errors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.senac.aesthetics.domains.enums.TipoMensagemEnum;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(Exception.class)
+  protected ResponseEntity<ErroGenerico> handleException(Exception ex) {
+    ErroGenerico erro = new ErroGenerico(Arrays.asList("Erro Interno no Servidor! \n Entre em Contato a Equipe do Aesthetics em service-desk@aesthetics.com!"),
+        TipoMensagemEnum.ERROR);
+    return ResponseEntity.internalServerError().body(erro);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  protected ResponseEntity<ErroGenerico> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException ex) {
+    List<String> erros = new ArrayList<String>();
+    for (ObjectError erro : ex.getBindingResult().getAllErrors()) {
+      erros.add(erro.getDefaultMessage());
+    }
+    ErroGenerico erro = new ErroGenerico(erros, TipoMensagemEnum.ERROR);
+    return ResponseEntity.internalServerError().body(erro);
+  }
+
+}
