0098cb8963b738bb1e8370fac7fb9f9871dc3cd4
leonardooliveira <leonardovo.iel@fieg.com.br>
2023-05-28 23:35:03 -0300

    Incluir a Tratativa de Erro Para a Integridade de Dados

--- Back-End/src/main/java/com/senac/aesthetics/errors/GlobalExceptionHandler.java
+++ Back-End/src/main/java/com/senac/aesthetics/errors/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,7 +18,9 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   protected ResponseEntity<ErroGenerico> handleException(Exception ex) {
-    ErroGenerico erro = new ErroGenerico(Arrays.asList("Erro Interno no Servidor! \n Entre em Contato a Equipe do Aesthetics em service-desk@aesthetics.com!"),
+    ErroGenerico erro = new ErroGenerico(
+        Arrays.asList(
+            "Erro Interno no Servidor! \n Entre em Contato a Equipe do Aesthetics em service-desk@aesthetics.com!"),
         TipoMensagemEnum.ERROR);
     return ResponseEntity.internalServerError().body(erro);
   }
@@ -33,4 +36,12 @@ public class GlobalExceptionHandler {
     return ResponseEntity.internalServerError().body(erro);
   }
 
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  protected ResponseEntity<ErroGenerico> handleDataIntegrityViolationException(
+      DataIntegrityViolationException ex) {
+    ErroGenerico erro = new ErroGenerico(
+        Arrays.asList("Não foi possível executar a ação pois o objeto esta ligado a outros objetos!"),
+        TipoMensagemEnum.ERROR);
+    return ResponseEntity.badRequest().body(erro);
+  }
 }
