fc26de2666904102ee795c618558753c4e9c3394
Marina Martins <marinadesouzamartins0@gmail.com>
2023-06-08 10:23:59 -0300

    componente fornecedor

--- Front-End/src/app/fornecedores/fornecedores.component.ts
+++ Front-End/src/app/fornecedores/fornecedores.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Fornecedor } from './../shared/domains/fornecedor.model';
+import { Component, ViewChild } from '@angular/core';
+import { TabelaFornecedoresComponent } from './tabela-fornecedores/tabela-fornecedores.component';
+import { FormularioFornecedoresComponent } from './formulario-fornecedores/formulario-fornecedores.component';
 
 @Component({
   selector: 'app-fornecedores',
   templateUrl: './fornecedores.component.html',
   styles: [],
 })
-export class FornecedoresComponent {}
+export class FornecedoresComponent {
+  @ViewChild(FormularioFornecedoresComponent) formularioFornecedores!: FormularioFornecedoresComponent;
+	@ViewChild(TabelaFornecedoresComponent) tabelaFornecedores!: TabelaFornecedoresComponent;
+
+	exibirFormularioFornecedores(fornecedor: Fornecedor) {
+		this.formularioFornecedores.fornecedor = JSON.parse(JSON.stringify(fornecedor));
+		this.formularioFornecedores.exibirFormulario = true;
+	}
+
+	atualizarTabela() {
+		this.tabelaFornecedores.atualizarTabela();
+	}
+
+}
