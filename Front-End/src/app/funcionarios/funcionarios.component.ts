8bf2221ba754da9a419a974c9bda35ec62e91cf4
Marina Martins <marinadesouzamartins0@gmail.com>
2023-06-07 22:36:58 -0300

    componente funcionario finalizado.

--- Front-End/src/app/funcionarios/funcionarios.component.ts
+++ Front-End/src/app/funcionarios/funcionarios.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TabelaFuncionariosComponent } from './tabela-funcionarios/tabela-funcionarios.component';
+import { FormularioFuncionariosComponent } from './formulario-funcionarios/formulario-funcionarios.component';
+import { Funcionario } from '../shared/domains/funcionario.model';
 
 @Component({
   selector: 'app-funcionarios',
   templateUrl: './funcionarios.component.html',
   styles: [],
 })
-export class FuncionariosComponent {}
+export class FuncionariosComponent {
+
+  @ViewChild(FormularioFuncionariosComponent) formularioFuncionarios!: FormularioFuncionariosComponent;
+	@ViewChild(TabelaFuncionariosComponent) tabelaFuncionarios!: TabelaFuncionariosComponent;
+
+	exibirFormularioFuncionario(funcionario: Funcionario) {
+		this.formularioFuncionarios.funcionario = JSON.parse(JSON.stringify(funcionario));
+		this.formularioFuncionarios.exibirFormulario = true;
+	}
+
+	atualizarTabela() {
+		this.tabelaFuncionarios.atualizarTabela();
+	}
+}
