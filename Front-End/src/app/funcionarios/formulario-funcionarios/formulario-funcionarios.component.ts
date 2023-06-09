c8d7e3ba97ebf7d6ed683cf3c72896ec8000b41f
leonardooliveira <leonardovo.iel@fieg.com.br>
2023-06-09 17:38:41 -0300

    Atualização do Componente do Funcionário

--- Front-End/src/app/funcionarios/formulario-funcionarios/formulario-funcionarios.component.ts
+++ Front-End/src/app/funcionarios/formulario-funcionarios/formulario-funcionarios.component.ts
@@ -1,8 +1,89 @@
-import { Component } from '@angular/core';
-
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Funcionario } from 'src/app/shared/domains/funcionario.model';
+import { Pessoa } from 'src/app/shared/domains/pessoa.model';
+import { FuncionarioService } from 'src/app/shared/services/funcionario.service';
+import { MensagensGenericasService } from 'src/app/shared/services/utils/mensagens-genericas.service';
+import { EstadosBrasileirosEnum } from 'src/app/shared/domains/enums/estados-brasileiros.enum';
+import { ValidacaoCamposService } from 'src/app/shared/services/utils/validacao-campos.service';
+import { TipoPessoaEnum } from 'src/app/shared/domains/enums/tipo-pessoa.enum';
 @Component({
-  selector: 'app-formulario-funcionarios',
-  templateUrl: './formulario-funcionarios.component.html',
-  styles: [],
+	selector: 'app-formulario-funcionarios',
+	templateUrl: './formulario-funcionarios.component.html',
+	styles: [],
 })
-export class FormularioFuncionariosComponent {}
+export class FormularioFuncionariosComponent implements OnInit {
+	exibirFormulario: Boolean = false;
+	estadosBrasileiro!: string[];
+	tiposPessoas!: string[];
+	mascaraCpfCnpj: string = '999.999.999-99';
+	mascaraTelefone: string = '(99)99999-9999';
+
+	@Input() funcionario: Funcionario = new Funcionario();
+	@Output() atualizarTabela: EventEmitter<void> = new EventEmitter();
+
+	constructor(private funcionarioService: FuncionarioService, private mensagensGenericasService: MensagensGenericasService, public validacaoCamposService: ValidacaoCamposService) {}
+
+	ngOnInit(): void {
+		this.estadosBrasileiro = Object.keys(EstadosBrasileirosEnum).map((value) => value);
+		this.tiposPessoas = Object.keys(TipoPessoaEnum).map((value) => value);
+	}
+
+	salvarFuncionario() {
+		if (!this.funcionario.comissao) {
+			this.funcionario.comissao = 0;
+		}
+
+		if (this.funcionario.id) {
+			this.atualizarFuncionario();
+		} else {
+			this.inserirFuncionario();
+		}
+	}
+
+	atualizarFuncionario() {
+		this.funcionarioService.atualizar(this.funcionario).subscribe({
+			next: () => {
+				this.mensagensGenericasService.mensagemPadraoDeSucesso('Funcionário', 'Atualizado');
+				this.atualizarTabela.emit();
+				this.atualizarTabelaEFecharFormulario();
+			},
+			error: (erro: HttpErrorResponse) => {
+				this.mensagensGenericasService.mensagemPadraoDeErro(erro);
+			},
+		});
+	}
+
+	inserirFuncionario() {
+		this.funcionarioService.inserir(this.funcionario).subscribe({
+			next: () => {
+				this.mensagensGenericasService.mensagemPadraoDeSucesso('Funcionário', 'Inserido');
+				this.atualizarTabela.emit();
+				this.atualizarTabelaEFecharFormulario();
+			},
+			error: (erro: HttpErrorResponse) => {
+				this.mensagensGenericasService.mensagemPadraoDeErro(erro);
+			},
+		});
+	}
+
+	atualizarTabelaEFecharFormulario() {
+		this.atualizarTabela.emit();
+		this.exibirFormulario = false;
+	}
+
+	modificarMascaraCpfOuCnpj() {
+		if (this.funcionario.pessoa.tipoPessoa == TipoPessoaEnum.PESSOA_FISICA) {
+			this.mascaraCpfCnpj = '999.999.999-99';
+		} else {
+			this.mascaraCpfCnpj = '99.999.999/9999-99';
+		}
+	}
+
+	funcionarioInvalido() {
+		if (this.funcionario.pessoa.nome && this.funcionario.pessoa.tipoPessoa && this.funcionario.pessoa.cpfOuCnpj && this.funcionario.pessoa.telefone) {
+			return false;
+		}
+		return true;
+	}
+}
--- Front-End/src/app/funcionarios/tabela-funcionarios/tabela-funcionarios.component.ts
+++ Front-End/src/app/funcionarios/tabela-funcionarios/tabela-funcionarios.component.ts
@@ -1,8 +1,79 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Table, TableLazyLoadEvent } from 'primeng/table';
+import { Funcionario } from 'src/app/shared/domains/funcionario.model';
+import { Pessoa } from 'src/app/shared/domains/pessoa.model';
+import { FuncionarioService } from 'src/app/shared/services/funcionario.service';
+import { MensagensGenericasService } from 'src/app/shared/services/utils/mensagens-genericas.service';
+import { EstadosBrasileirosEnum } from 'src/app/shared/domains/enums/estados-brasileiros.enum';
+
 
 @Component({
   selector: 'app-tabela-funcionarios',
   templateUrl: './tabela-funcionarios.component.html',
   styles: [],
 })
-export class TabelaFuncionariosComponent {}
+export class TabelaFuncionariosComponent {
+
+  funcionarios!: Funcionario[];
+	funcionariosSelecionados!: Funcionario[];
+	quantidadeTotalFuncionarios!: number;
+	quantidadeFuncionariosExibidosPorPagina: number = 30;
+
+	@Output() exibirFormularioFuncionario: EventEmitter<Funcionario> = new EventEmitter<Funcionario>();
+
+	@ViewChild(Table) private tabelaFuncionarios!: Table;
+
+	constructor(private funcionarioService: FuncionarioService, private mensagensGenericasService: MensagensGenericasService) {}
+
+	ngOnInit(): void {
+		this.obterTodosFuncionarios(0, 30);
+	}
+
+  obterTodosFuncionarios(numeroPagina: number, quantidadePorPagina: number, ordenarPor?: string): void {
+		this.funcionarioService.obterTodosPorPagina(numeroPagina, quantidadePorPagina, ordenarPor).subscribe({
+			next: (resposta) => {
+				this.funcionarios = resposta.content;
+				this.quantidadeTotalFuncionarios = resposta.totalElements;
+			},
+			error: (erro: HttpErrorResponse) => {
+				this.mensagensGenericasService.mensagemPadraoDeErro(erro);
+			},
+		});
+	}
+
+	mostrarFormularioFuncionarios(funcionario?: Funcionario) {
+		if (funcionario) {
+			this.exibirFormularioFuncionario.emit(funcionario);
+		} else {
+			this.exibirFormularioFuncionario.emit(new Funcionario());
+		}
+	}
+
+	excluirFuncionario(idFuncionario: number) {
+		this.funcionarioService.excluir(idFuncionario).subscribe({
+			next: () => {
+				this.mensagensGenericasService.mensagemPadraoDeSucesso('Funcionário', 'excluído');
+				this.atualizarTabela();
+			},
+			error: (erro: HttpErrorResponse) => {
+				this.mensagensGenericasService.mensagemPadraoDeErro(erro);
+			},
+		});
+	}
+
+	atualizarTabela() {
+		this.obterTodosFuncionarios(Math.floor(Number(this.tabelaFuncionarios.first) / Number(this.tabelaFuncionarios.rows)), Number(this.tabelaFuncionarios._rows));
+	}
+
+	mudarPagina(evento: TableLazyLoadEvent) {
+		if (evento.first != undefined && evento.rows != undefined) {
+			this.obterTodosFuncionarios(Math.floor(evento.first / evento.rows), evento.rows, 'id');
+		}
+	}
+
+	obterEstadoBrasileiro(valor: string) {
+		return Object.values(EstadosBrasileirosEnum)[Object.keys(EstadosBrasileirosEnum).indexOf(valor)];
+	}
+
+}
