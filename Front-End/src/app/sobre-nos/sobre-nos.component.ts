f040d8a06587d9849c15c68b9ee09741004835e5
leonardooliveira <leonardovo.iel@fieg.com.br>
2023-06-12 11:23:15 -0300

    Atualização do Componente Sobre Nós

--- Front-End/src/app/sobre-nos/sobre-nos.component.ts
+++ Front-End/src/app/sobre-nos/sobre-nos.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-sobre-nos',
 	templateUrl: './sobre-nos.component.html',
-	styles: [],
+	styleUrls: ['./sobre-nos.component.less'],
 })
 export class SobreNosComponent {
 	redirecionar(link: string) {
